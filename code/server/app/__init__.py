cc2189a70236c5309ba736b93e1789d346ed977b
Guy Royse <guy@guyroyse.com>
2018-09-27 13:32:57 -0400

    better server code

--- code/server/app/__init__.py
+++ code/server/app/__init__.py
@@ -2,14 +2,17 @@ from flask import Flask, request, jsonify
 
 from .classinator import BigfootClassinatorModel
 
+APP_NAME = "Bigfoot Classinator"
+APP_VERSION = "0.0.1"
+
 def create_app(test_config=None):
   app = Flask(__name__, instance_relative_config=True)
 
   @app.route('/version')
   def version():
     return jsonify({
-      'app'     : "Bigfoot Classinator",
-      'version' : "0.0.1"
+      'app'     : APP_NAME,
+      'version' : APP_VERSION
     })
 
   @app.route('/classinate', methods=['POST'])
@@ -20,8 +23,8 @@ def create_app(test_config=None):
     classination = model.predict(sighting)
 
     return jsonify({
-      'app'          : "Bigfoot Classinator",
-      'version'      : "0.0.1",
+      'app'          : APP_NAME,
+      'version'      : APP_VERSION,
       'classination' : classination.to_dict(),
       'sighting'     : sighting
     })
