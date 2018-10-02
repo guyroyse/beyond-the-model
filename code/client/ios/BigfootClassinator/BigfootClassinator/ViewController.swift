31d9ed39e682dd0f6eef6ead90396b094af195ac
Guy Royse <guy@guyroyse.com>
2018-10-02 16:43:52 -0400

    an empty xcode project

---
+++ code/client/ios/BigfootClassinator/BigfootClassinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BigfootClassinator
+//
+//  Created by Guy Royse on 10/2/18.
+//  Copyright © 2018 Nexosis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
