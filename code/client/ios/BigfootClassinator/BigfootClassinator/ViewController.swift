ae61b18ed5549f5a5dc9a3123fc65126fe78666c
Guy Royse <guy@guyroyse.com>
2018-10-04 17:45:45 -0400

    keyboard more well behaved

--- code/client/ios/BigfootClassinator/BigfootClassinator/ViewController.swift
+++ code/client/ios/BigfootClassinator/BigfootClassinator/ViewController.swift
@@ -1,8 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var sightingTextView: UITextView!
+    @IBOutlet weak var classinateButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     private var classinator = Classinator.shared
     private var messages = Messages.shared
@@ -11,17 +13,59 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            sightingTextView.resignFirstResponder()
+            return false
+        }
+
+        return true
+    }
+
     @IBAction func classinateButtonTapped(_ sender: Any) {
         let sighting: String = self.sightingTextView?.text ?? ""
+
+        startActivityIndication()
+
         _ = classinator.classinate(sighting: sighting)
             .done { classination in
-                let title = classination.rawValue
-                let message = self.messages.fetchMessage(classination: classination)
-                self.showAlert(title: title, message: message)
+                self.clearInput()
+                self.showResults(classination: classination)
+            }
+            .ensure {
+                self.stopActivityIndication()
             }
             .catch { error in
-                self.showAlert(title: "Error", message: error.localizedDescription)
+                self.showError(error: error)
             }
+    }
+
+    private func clearInput() {
+        sightingTextView.text = ""
+    }
+
+    private func showResults(classination: Classination) {
+        let title = classination.rawValue
+        let message = self.messages.fetchMessage(classination: classination)
+        showAlert(title: title, message: message)
+    }
+
+    private func showError(error: Error) {
+        self.showAlert(title: "Error", message: error.localizedDescription)
+    }
+
+    private func startActivityIndication() {
+        sightingTextView.isEditable = false
+        sightingTextView.isSelectable = false
+        classinateButton.isEnabled = false
+        activityIndicator.startAnimating()
+    }
+
+    private func stopActivityIndication() {
+        activityIndicator.stopAnimating()
+        classinateButton.isEnabled = true
+        sightingTextView.isSelectable = true
+        sightingTextView.isEditable = true
     }
 
     private func showAlert(title: String, message: String) {
@@ -29,6 +73,5 @@ class ViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true, completion: nil)
     }
-
 }
 
